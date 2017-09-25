16547a3fb9ab4c94872b4b623ec4a40a3e308d31
Brian Chang <kyoukamu@gmail.com>
2017-09-25 14:04:22 -0700

    added logic

--- apps/ninjagold/urls.py
+++ apps/ninjagold/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index),
-	url(r'^process_money$', views.process),
+	url(r'^process_money/(?P<building_type>\w+)', views.process),
 ]
--- apps/ninjagold/views.py
+++ apps/ninjagold/views.py
@@ -1,10 +1,45 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+import random
 
 def index(request):
+	try:
+		request.session['gold_amount']
+	except KeyError:
+		request.session['gold_amount'] = 0
+
+	try:
+		request.session['activitylist']
+	except KeyError:
+		request.session['activitylist'] = []
+
 	return render(request, 'ninjagold/index.html')
 
-def process(request):
+def process(request, building_type):
+	if building_type == "farm":
+		earnedgold = random.randint(10,21)
+		request.session['gold_amount'] += earnedgold
+		message = "Earned "+str(earnedgold)+" gold from the farm!"
+		request.session['activitylist'].insert(0,message)
+
+	elif building_type == "cave":
+		earnedgold = random.randint(5,11)
+		request.session['gold_amount'] += earnedgold
+		message = "Earned "+str(earnedgold)+" gold from the cave!"
+		request.session['activitylist'].insert(0,message)
+
+	elif building_type == "house":
+		earnedgold = random.randint(2,6)
+		request.session['gold_amount'] += earnedgold
+		message = "Earned "+str(earnedgold)+" gold from the house!"
+		request.session['activitylist'].insert(0,message)
+
+	elif building_type == "casino":
+		earnedgold = random.randint(0,51)
+		request.session['gold_amount'] += earnedgold
+		message = "Earned "+str(earnedgold)+" gold from the casino!"
+		request.session['activitylist'].insert(0,message)
 	return redirect('/')
